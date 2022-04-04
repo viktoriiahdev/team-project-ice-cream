75ce1ecf7ab4760a43453fa64fcec692872914ce
Viktoriia Hrytsenko <dev.viktoriia.h@gmail.com>
2022-04-05 01:04:53 +0300

    move js script for gallery

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,32 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+// modal gallery
+const backdrop = document.querySelector('[data-backdrop-gallery]');
+const modal = document.querySelector('[data-modal-gallery]');
+const modalImg = document.querySelector('[data-modal-image]');
+
+const images = Array.from(document.getElementsByClassName('gallery__image'));
+
+const hiddenClass = 'backdrop--is-hidden';
+
+images.forEach(img => {
+  img.onclick = function () {
+    console.log('click');
+    backdrop.classList.remove(hiddenClass);
+    modalImg.src = img.src;
+  };
+});
+
+backdrop.addEventListener('click', closeImage);
+
+function openImage(img) {
+  console.log('click');
+  backdrop.classList.remove(hiddenClass);
+  modalImg.src = img.src;
+}
+
+function closeImage() {
+  if (!this.classList.contains('gallery__image')) backdrop.classList.add(hiddenClass);
+}
